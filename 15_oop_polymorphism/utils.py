11fae079596c5e97cfe52b644a2cf5a1bfcbf858
Rickard Garnau <rickard.garnau89@gmail.com>
2026-02-07 11:17:17 +0100

    Cleaned up project structure: Geometry OOP Library

--- 15_oop_polymorphism/utils.py
+++
@@ -1,5 +0,0 @@
-from numbers import Number
-
-def validate_number(value):        
-    if not isinstance(value, Number):
-        raise TypeError(f"value must be number not {type(value)}")
--- exercise_6/plotter.py
+++
@@ -1,6 +0,0 @@
-np.random.seed(42)
-x = np.linspace(0, 10, 100)
-y = 2 * x + 5 + np.random.normal(0, 2, size=len(x))
-
-annotate = AnnotatePlot(fig, ax)
-annotate.arrow("Big dip here", (8, 10), (7.5, 14.8))
